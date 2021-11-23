71f6f375bc1d61fcf98c91bb7b59e130067a8a20
Jan Früchtl <jan@fruechtl.me>
2021-11-23 18:51:26 +0100

    Add better caption support for galleries

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -1,17 +1,19 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
 
 <div class="wrapper">
-  <figure class="case-study__gallery">
-    <ul class="case-study__gallery-list">
+  <div class="case-study__gallery">
+    <div class="case-study__gallery-list">
     <?php foreach ($block->images()->toFiles() as $image): ?>
-      <li class="case-study__gallery-image">
+      <figure class="case-study__gallery-image">
         <img srcset="<?= $image->srcset([400, 800, 1000, 1500]) ?>" src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
         
         <?php if ($image->caption()->isNotEmpty()): ?>
-        <span class="case-study__gallery-caption"><?= $image->caption() ?></span>
+        <figcaption class="case-study__gallery-caption">
+          <?= $image->caption() ?>
+        </figcaption>
         <?php endif ?>
-      </li>
+      </figure>
     <?php endforeach ?>
-    </ul>
-  </figure>
+    </div>
+  </div>
 </div>
