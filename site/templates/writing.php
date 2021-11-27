dee90621736dd166a66338b7bb952beff774df9c
Jan Früchtl <jan@fruechtl.me>
2021-11-27 13:54:08 +0100

    Update writing markup structure

--- site/templates/writing.php
+++ site/templates/writing.php
@@ -9,11 +9,11 @@
       foreach($groupedItems->flip() as $year => $itemsPerYear): 
         ?>
     <div class="writing-block">
-      <h2 class="title-3"><?= $year ?></h2>
+      <h3 class="title-3"><?= $year ?></h3>
       <ul class="writing__list">
         <?php foreach($itemsPerYear->flip() as $item) : ?>
           <li class="writing__item">
-            <h3 class="writing__item-title title-2"><a href="<?= $item->url() ?>"><?= $item->title() ?></a></h3>
+            <h4 class="writing__item-title title-2"><a href="<?= $item->url() ?>"><?= $item->title() ?></a></h4>
             <time class="writing__item-caption"><?= $item->date()->toDate('l, F jS Y') ?></time>
           </li>
           <?php endforeach; ?>
