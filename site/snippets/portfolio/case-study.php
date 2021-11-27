b26c5905c98fce16e8fc033b885015e2882828b0
Jan Früchtl <jan@fruechtl.me>
2021-11-27 13:58:05 +0100

    Update markup

--- site/snippets/portfolio/case-study.php
+++ site/snippets/portfolio/case-study.php
@@ -1,6 +1,6 @@
 <a class="project-item" href="<?= $data->url() ?>">
   <div class="project-content">
-    <h2 class="project-title title-2"><?= $data->title() ?></h2>
+    <h3 class="project-title title-2"><?= $data->title() ?></h3>
     <p class="project-description"><?= $data->intro() ?></p>
   </div>
   <span class="project-details"><?= $data->year() ?></span>
