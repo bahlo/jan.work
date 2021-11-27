36a5ff7572521b478ec890a31c5f29f7a2472379
Jan Früchtl <jan@fruechtl.me>
2021-11-27 13:54:37 +0100

    Update article markup

--- site/templates/article.php
+++ site/templates/article.php
@@ -3,7 +3,7 @@
 <article class="post">
   <div class="wrapper wrapper--small">
     <div class="post-header">
-      <h1 class="post-title title-1"><?= $page->title() ?></h1>
+      <h2 class="post-title title-1"><?= $page->title() ?></h2>
       <time class="post-date title-6"><?= $page->date()->toDate('l, F jS Y') ?></time>
     </div>
     <div class="post-content"><?= $page->text()->kirbytext() ?></div>
